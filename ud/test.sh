b5fdd7b7370d28a52ca30c662821a31520ca8067
Kevin Scannell <kscanne@gmail.com>
2021-01-15 10:25:47 -0600

    simplify testing to generated conllu

--- ud/test.sh
+++ ud/test.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
-TMPCONLLU=`mktemp`
-
-usage() {
-	echo "Usage: bash test.sh [-g|-p]"
-	echo "-g to test gold.tsv; -p to test pending additions in pedit.tsv"
-	rm -f "${TMPCONLLU}"
-	exit 1
-}
-
-if [ $# -ne 1 ]
-then
-	usage
-fi
-
-ANSEO=${HOME}/gaeilge/ga2gv/ga2gv/ud
-
-if [ "${1}" = "-p" ]
-then
-	cat ${ANSEO}/pedit.tsv | perl todist.pl > ${TMPCONLLU}
-else
-	if [ "${1}" = "-g" ]
-	then
-		cp ${ANSEO}/gv_cadhan-ud-test.conllu ${TMPCONLLU}
-	else
-		usage
-	fi
-fi
+CONLLU=${HOME}/gaeilge/ga2gv/ga2gv/ud/gv_cadhan-ud-test.conllu
+echo "Sentence count:" `cat ${CONLLU} | egrep '^# sent_id' | wc -l`
+echo "Word count:" `cat ${CONLLU} | egrep '^[0-9]+[^-]' | wc -l`
 cd ${HOME}/seal/temp/udtools
 source ${HOME}/gaeilge/claochlu/env/bin/activate
-python validate.py --lang=gv "${TMPCONLLU}"
-rm -f "${TMPCONLLU}"
+python validate.py --lang=gv "${CONLLU}"
