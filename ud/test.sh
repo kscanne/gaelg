21530246d7132b523f77071cccf220e52df07892
Kevin Scannell <kscanne@gmail.com>
2021-04-24 17:22:01 -0500

    add features to plural possessives

--- ud/test.sh
+++ ud/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 CONLLU=${HOME}/gaeilge/ga2gv/ga2gv/ud/all.conllu
+make all.conllu
 echo "Sentence count:" `cat ${CONLLU} | egrep '^# sent_id' | wc -l`
 echo "Word count:" `cat ${CONLLU} | egrep '^[0-9]+[^-]' | wc -l`
 cd ${HOME}/seal/temp/udtools
