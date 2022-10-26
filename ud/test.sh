4bad25612336d8a7106d14bc6a803eb792722845
Kevin Scannell <kscanne@gmail.com>
2022-10-26 13:41:50 -0500

    test both train and test

--- ud/test.sh
+++ ud/test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-CONLLU=${HOME}/gaeilge/ga2gv/ga2gv/ud/all.conllu
-make all.conllu
-echo "Sentence count:" `cat ${CONLLU} | egrep '^# sent_id' | wc -l`
-echo "Word count:" `cat ${CONLLU} | egrep '^[0-9]+[^-]' | wc -l`
+
+# pass "test" or "train"
+testone() {
+	CONLLU=${HOME}/gaeilge/ga2gv/ga2gv/ud/gv_cadhan-ud-${1}.conllu
+	echo "Checking gv_cadhan-ud-${1}.conllu...."
+	echo "Sentence count:" `cat ${CONLLU} | egrep '^# sent_id' | wc -l`
+	echo "Word count:" `cat ${CONLLU} | egrep '^[0-9]+[^-]' | wc -l`
+	python validate.py --lang=gv "${CONLLU}"
+}
+
 cd ${HOME}/seal/temp/udtools
 source ${HOME}/gaeilge/claochlu/env/bin/activate
-python validate.py --lang=gv "${CONLLU}"
+testone test
+testone train
