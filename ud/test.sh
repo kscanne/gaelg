6f493078b03f0996ae6292faa63684123a2f6c4b
Kevin Scannell <kscanne@gmail.com>
2024-05-09 17:19:59 +0000

    point at github repo

--- ud/test.sh
+++ ud/test.sh
@@ -2,14 +2,14 @@
 
 # pass "test" or "train"
 testone() {
-	CONLLU=${HOME}/gaeilge/ga2gv/ga2gv/ud/gv_cadhan-ud-${1}.conllu
+	CONLLU=${HOME}/seal/UD_Manx-Cadhan/gv_cadhan-ud-${1}.conllu
 	echo "Checking gv_cadhan-ud-${1}.conllu...."
 	echo "Sentence count:" `cat ${CONLLU} | egrep '^# sent_id' | wc -l`
 	echo "Word count:" `cat ${CONLLU} | egrep '^[0-9]+[^-]' | wc -l`
 	python validate.py --lang=gv "${CONLLU}"
 }
 
-cd ${HOME}/seal/temp/udtools
-source ${HOME}/gaeilge/claochlu/env/bin/activate
+cd ${HOME}/seal/clones/tools
+source ${HOME}/env/bin/activate
 testone test
 testone train
